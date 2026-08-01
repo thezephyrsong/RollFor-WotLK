2e6114c11893cd9070be2092e2c9c9afe577760b
ZephZae <116720216+thezephyrsong@users.noreply.github.com>
2026-08-02 08:46:19 +1000

    Update BossList.lua

--- src/BossList.lua
+++ src/BossList.lua
@@ -12,45 +12,6 @@ M.zones    = {
   [ "Durotar" ] = {
     "Elder Mottled Boar"
   },
-  [ "Emerald Sanctum" ] = {
-    "Erennius",
-    "Solnius"
-  },
-  [ "Tower of Karazhan" ] = {
-    "Keeper Gnarlmoon",
-    "Ley-Watcher Incantagos",
-    "Anomalus",
-    "Echo of Medivh",
-    "King",
-    "Queen",
-    "Bishop",
-    "Rook",
-    "Ima'ghaol, Herald of Desolation",
-    "Sanv Tas'dal",
-    "Rupturan the Broken",
-    "Kruul",
-    "Mephistroth"
-  },
-  [ "Lower Karazhan Halls" ] = {
-    "Master Blacksmith Rolfen",
-    "Brood Queen Araxxna",
-    "Grizikil",
-    "Clawlord Howlfang",
-    "Lord Blackwald II",
-    "Moroes"
-  },
-  [ "Timbermaw Hold" ] = {
-    "Karrsh the Sentinel",
-    "Rotgrowl",
-    "Archdruid Kronn",
-    "Loktanag the Vile",
-    "Ormanos the Cracked",
-    "Trioch the Devourer",
-    "Selenaxx Foulheart",
-    "Chieftain Partath",
-    "Ursol",
-    "Peroth'arn"
-  },
   [ "Zul'Gurub" ] = {
     "High Priestess Jeklik",
     "High Priest Venoxis",
@@ -145,6 +106,109 @@ M.zones    = {
     "Sir Zeliek",
     "Sapphiron",
     "Kel'Thuzad"
+  },
+  -- Zone keys below are unverified -- Ascension has been known to split or
+  -- rename instance zone text (see "Tower of Karazhan" / "Lower Karazhan
+  -- Halls" above, a custom low-level Karazhan distinct from the endgame one
+  -- below). Confirm each key with /dump GetRealZoneText() in-game and fix
+  -- the key if it doesn't match.
+  [ "Karazhan" ] = {
+    "Attumen the Huntsman",
+    "Moroes",
+    "Maiden of Virtue",
+    "Opera Event",
+    "The Curator",
+    "Shade of Aran",
+    "Netherspite",
+    "Chess Event",
+    "Prince Malchezaar"
+  },
+  [ "Gruul's Lair" ] = {
+    "High King Maulgar",
+    "Gruul the Dragonkiller"
+  },
+  [ "Magtheridon's Lair" ] = {
+    "Magtheridon"
+  },
+  [ "Serpentshrine Cavern" ] = {
+    "Hydross the Unstable",
+    "The Lurker Below",
+    "Leotheras the Blind",
+    "Fathom-Lord Karathress",
+    "Morogrim Tidewalker",
+    "Lady Vashj"
+  },
+  [ "Tempest Keep" ] = {
+    "Al'ar",
+    "Void Reaver",
+    "High Astromancer Solarian",
+    "Kael'thas Sunstrider"
+  },
+  [ "Zul'Aman" ] = {
+    "Akil'zon",
+    "Nalorakk",
+    "Jan'alai",
+    "Halazzi",
+    "Hex Lord Malacrass",
+    "Zul'jin"
+  },
+  [ "Black Temple" ] = {
+    "High Warlord Naj'entus",
+    "Supremus",
+    "Shade of Akama",
+    "Teron Gorefiend",
+    "Gurtogg Bloodboil",
+    "Reliquary of Souls",
+    "Mother Shahraz",
+    "The Illidari Council",
+    "Illidan Stormrage"
+  },
+  [ "Sunwell Plateau" ] = {
+    "Kalecgos",
+    "Brutallus",
+    "Felmyst",
+    "Eredar Twins",
+    "M'uru",
+    "Kil'jaeden"
+  },
+  [ "Ulduar" ] = {
+    "Flame Leviathan",
+    "Ignis the Furnace Master",
+    "Razorscale",
+    "XT-002 Deconstructor",
+    "Steelbreaker",
+    "Runemaster Molgeim",
+    "Stormcaller Brundir",
+    "Kologarn",
+    "Auriaya",
+    "Hodir",
+    "Thorim",
+    "Freya",
+    "Mimiron",
+    "General Vezax",
+    "Yogg-Saron",
+    "Algalon the Observer"
+  },
+  [ "Trial of the Crusader" ] = {
+    "Northrend Beasts",
+    "Lord Jaraxxus",
+    "Faction Champions",
+    "Val'kyr Twins",
+    "Anub'arak"
+  },
+  [ "Icecrown Citadel" ] = {
+    "Lord Marrowgar",
+    "Lady Deathwhisper",
+    "Gunship Battle",
+    "Deathbringer Saurfang",
+    "Festergut",
+    "Rotface",
+    "Professor Putricide",
+    "Blood Prince Council",
+    "Blood-Queen Lana'thel",
+    "Valithria Dreamwalker",
+    "Sindragosa",
+    "The Lich King"
   }
 }
 
