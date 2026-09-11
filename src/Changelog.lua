1476d785c75abc5a2d00ae9a8231303dbe536245
ZephZae <116720216+thezephyrsong@users.noreply.github.com>
2026-09-11 15:31:35 +1000

    Update Changelog with new version entries

--- src/Changelog.lua
+++ src/Changelog.lua
@@ -9,7 +9,8 @@ if m.Changelog then return end
 local M = {}
 
 M.entries = {
-  { ver = "1.2.4", text = "Fixes for issues around combat rolling" },
+  { ver = "1.2.6", text = "Fix capitalisation and improve Boss List to include all bosses included in stock 3.3.5a" },
+  { ver = "1.2.5", text = "Fixes for issues around combat rolling" },
   { ver = "1.2.4", text = "Added Ascension/Bisbeard referral links and Bug fixes." },
   { ver = "1.2.3", text = "Track winners of Blizzard's built-in group-loot Need/Greed/Disenchant rolls into the same award history as SR/MS/OS." },
   { ver = "1.2.2", text = "Bug fix and seperate changelog" },
